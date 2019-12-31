b8d0ac83cec3048151f26197c21fd85710bb5fa0
karlo <deguzmankarlo@gmail.com>
2019-12-31 12:24:07 +0000

    Refactor class instantiation into let statement before tests

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,14 +1,14 @@
 require "./lib/tic_tac_toe"
 
 describe "tic tac toe" do
+  let(:ttt) { ttt = TicTacToe.new }
+
   it "allows an X to be placed on the 3x3 board" do
-    ttt = TicTacToe.new
     ttt.move("X", 1)
     expect(ttt.board).to include("X")
   end
 
   it "raises an error if not X or O" do
-    ttt = TicTacToe.new
     expect{ ttt.move("Y", 0) }.to raise_error("Invalid character")
   end
 end
