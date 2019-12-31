70121b59c27a40bbc06bb3f6fc157b1249d9e6db
karlo <deguzmankarlo@gmail.com>
2019-12-31 12:16:41 +0000

    Raises error if incorrect character used

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,6 +8,7 @@ class TicTacToe
   end
 
   def move(x_o, pos)
+    raise "Invalid character" if x_o != "X" && x_o != "O"
     @board[pos] = x_o
   end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -3,8 +3,12 @@ require "./lib/tic_tac_toe"
 describe "tic tac toe" do
   it "allows an X to be placed on the 3x3 board" do
     ttt = TicTacToe.new
-    ttt.move('X', 1)
-    p ttt.board
-    expect(ttt.board).to include('X')
+    ttt.move("X", 1)
+    expect(ttt.board).to include("X")
+  end
+
+  it "raises an error if not X or O" do
+    ttt = TicTacToe.new
+    expect{ ttt.move("Y", 0) }.to raise_error("Invalid character")
   end
 end
