b7830423f2df5791eb15f68a92c321872f0f82e3
karlo <deguzmankarlo@gmail.com>
2020-01-03 15:52:07 +0000

    Change board position naming to 1 to 9 from 0 to 8

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,7 +10,7 @@ class TicTacToe
 
   def move(x_o, pos)
     valid?(x_o, pos)
-    @board[pos] = x_o
+    @board[pos - 1] = x_o
     @last_move = x_o
     print_board
     end_game?
@@ -33,8 +33,8 @@ class TicTacToe
 
   def valid?(x_o, pos)
     raise "Invalid character" unless ["X", "O"].include?(x_o)
-    raise "Out of bounds" unless pos.between?(0, 8)
-    raise "Position already taken" unless @board[pos] == " "
+    raise "Out of bounds" unless pos.between?(1, 9)
+    raise "Position already taken" unless @board[pos - 1] == " "
     raise "Invalid move - next player's turn" if x_o == @last_move
   end
 
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -8,28 +8,28 @@ describe "tic tac toe" do
   end
 
   it "allows an X to be placed on the 3x3 board" do
-    ttt.move("X", 1)
+    ttt.move("X", 2)
     expect(ttt.board).to include("X")
   end
 
   describe "raises an error" do
     it "if not X or O" do
-      expect{ ttt.move("Y", 0) }.to raise_error("Invalid character")
+      expect{ ttt.move("Y", 1) }.to raise_error("Invalid character")
     end
 
     it "if placing move out of bounds" do
-      expect{ ttt.move("X", 9) }.to raise_error("Out of bounds")
+      expect{ ttt.move("X", 10) }.to raise_error("Out of bounds")
       expect{ ttt.move("X", -1) }.to raise_error("Out of bounds")
     end
 
     it "if position is already taken" do
-      ttt.move("X", 1)
-      expect{ ttt.move("O", 1) }.to raise_error("Position already taken")
+      ttt.move("X", 2)
+      expect{ ttt.move("O", 2) }.to raise_error("Position already taken")
     end
 
     it "if move out of turn" do
-      ttt.move("X", 1)
-      expect{ ttt.move("X", 2) }.to raise_error("Invalid move - next player's turn")
+      ttt.move("X", 2)
+      expect{ ttt.move("X", 3) }.to raise_error("Invalid move - next player's turn")
     end
   end
 
@@ -90,12 +90,12 @@ describe "tic tac toe" do
   end
 
   it "can output the winner to the user" do
-    ttt.move("X", 0)
-    ttt.move("O", 3)
     ttt.move("X", 1)
     ttt.move("O", 4)
+    ttt.move("X", 2)
+    ttt.move("O", 5)
     allow(ttt).to receive(:print).and_return("Game Over - X Wins!")
-    expect(ttt.move("X", 2)).to eq("Game Over - X Wins!")
+    expect(ttt.move("X", 3)).to eq("Game Over - X Wins!")
   end
 
   it "can print the game board" do
@@ -107,6 +107,6 @@ describe "tic tac toe" do
   it "can declare a tie game" do
     ttt.board = ["X", "X", "O", "O", "O", "X", "X", "O", " "]
     allow(ttt).to receive(:print).and_return("Tie Game!")
-    expect(ttt.move("X", 8)).to eq("Tie Game!")
+    expect(ttt.move("X", 9)).to eq("Tie Game!")
   end
 end
